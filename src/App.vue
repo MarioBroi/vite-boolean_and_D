4858a791de71247e7f161070a55ef566dbf6418d
MarioBroi <inoix16@gmail.com>
2024-06-05 16:34:59 +0200

    add AppFooter and import in App.vue page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script>
 import { RouterView } from "vue-router";
-import AppHeader from "./partials/AppHeader.vue";
 
+import AppHeader from "./partials/AppHeader.vue";
+import AppFooter from "./partials/AppFooter.vue";
 export default {
   name: 'App',
   components: {
     AppHeader,
+    AppFooter,
   }
 
 }
@@ -15,7 +17,6 @@ export default {
 
   <AppHeader />
   <RouterView />
+  <AppFooter />
 
 </template>
-
-<style scoped></style>
